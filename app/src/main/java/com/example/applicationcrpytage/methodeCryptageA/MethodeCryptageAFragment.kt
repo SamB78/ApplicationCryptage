d069d06f7f229b56a633305af2d1d778685502c6
Sam <bertrand.sam78@gmail.com>
2020-03-13 00:46:36 +0100

    D B1 B2 OK

--- app/src/main/java/com/example/applicationcrpytage/methodeCryptageA/MethodeCryptageAFragment.kt
+++ app/src/main/java/com/example/applicationcrpytage/methodeCryptageA/MethodeCryptageAFragment.kt
@@ -6,7 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.applicationcrpytage.R
+import com.example.applicationcrpytage.databinding.FragmentMethodeCryptageABinding
+import com.example.applicationcrpytage.databinding.FragmentMethodeCryptageB2Binding
+import com.example.applicationcrpytage.methodeCryptageB2.MethodeCryptageB2ViewModel
 
 /**
  * A simple [Fragment] subclass.
@@ -17,8 +23,19 @@ class MethodeCryptageAFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val binding: FragmentMethodeCryptageABinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_methode_cryptage_a, container, false)
+        binding.executePendingBindings()
+        binding.lifecycleOwner = this
+
+        val viewModel = ViewModelProvider(this).get(MethodeCryptageAViewModel::class.java)
+        binding.viewModel = viewModel
+
+        val navController = findNavController()
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_methode_cryptage_a, container, false)
+        return binding.root
     }
 
 
